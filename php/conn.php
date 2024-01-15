88b91f32f7b84a8e705928f135f088fa7bf6e39b
Daniel de Sousa <156535206+DanielDeSousaDEV@users.noreply.github.com>
2024-01-15 10:14:09 -0300

    Atualizar o conn.php

--- php/conn.php
+++ php/conn.php
@@ -1,1 +1,13 @@
-
+<?php
+try{
+    $host = "192.168.0.3";
+    $bdname = "Login";
+    $user = "root";
+    $password = "root";
+    
+    $conn = new PDO('mysql:host='.$host.';bdname='.$bdname, $user, $password);
+    
+}catch(PDOException $e){
+    echo "<strong>ERRO no PDO</strong>" . $e;
+}
+?>
